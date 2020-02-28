345641ddc4e633403cfd3ff5ab840daa9a696024
Dandush03 <d.laloush@outlook.com>
2020-02-28 13:44:05 -0400

    Fixed Log.rb File Rubocop Errors

--- lib/log.rb
+++ lib/log.rb
@@ -3,10 +3,11 @@
 require_relative '../lib/coloring.rb'
 require_relative '../lib/error.rb'
 
+# rubocop: disable Metrics/AbcSize:
+
 # Log Every Data Displayed
 class Log
-  attr_reader :log, :error_list
-  attr_accessor :logger
+  attr_reader :error_list
 
   def initialize
     @err = Error.new
@@ -14,20 +15,20 @@ class Log
     @error_list = "\n"
   end
 
-  def log(e)
-    e = e.split
-    @logger << @err.send(e[0],"#{e[1]}", "#{e[2]}") if e.length == 3
-    @logger << @err.send(e[0],"#{e[1]}", "#{e[2]}", "#{e[3]}", "#{e[4]}") if e.length == 5
+  def log(exp)
+    e = exp.split
+    @logger << @err.send(e[0], e[1], e[2]) if e.length == 3
+    @logger << @err.send(e[0], e[1], e[2], e[3], e[4]) if e.length == 5
   end
 
   def filter
     @logger.each do |log|
-      if log[0] == 1 && log[2] == 1
-        @logger.each do |item|
-          if item[0] == 2 && item[2] == 1
-            @logger = @logger.reject{|search| search == item}
-          end
-        end
+      next unless log[0] == 1 && log[2] == 1
+
+      @logger.each do |item|
+        next unless item[0] == 2 && item[2] == 1
+
+        @logger = @logger.reject { |search| search == item }
       end
     end
   end
@@ -45,3 +46,5 @@ class Log
     end
   end
 end
+
+# rubocop: enable Metrics/AbcSize:
