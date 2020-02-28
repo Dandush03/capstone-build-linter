657cf113d9ca4b7cee7d59fc9a79e0d598d33620
Dandush03 <d.laloush@outlook.com>
2020-02-27 22:13:41 -0400

    Added indentation def

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# capstone-build-linter
+# CAPSTONE - BUILD LINTER (CSS) - RUBY
+
+### Description of the project
+
+The porpuse of this project is to create a Linter that can the fallowing issues in a CSS file:
+
+-	Identation
+- Missing Brakets
+
--- lib/scanner.rb
+++ lib/scanner.rb
@@ -9,8 +9,15 @@ class Scanner
 	end
 
 	def indentation
+		indent = 0
+		counter = 0
 		@lines.each_with_index do |line, index|
-				puts line[/\A */].size
+			indent = line[/\A */].size
+			counter -= 1 if line.match(/}/)
+			if indent != counter * 2 
+				puts "DLint Indentation Error:\n  Line #{index + 1}: expected indent of #{counter * 2} not #{indent}" 
+			end
+			counter += 1 if line.match(/{/)
 		end
 	end
 end
