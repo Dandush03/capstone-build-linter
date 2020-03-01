79e6d23af57486802cfa1631c5cc28025cb3dac4
Dandush03 <d.laloush@outlook.com>
2020-02-29 22:21:35 -0400

    Fixing Gem Config

--- DRev.gemspec
+++ DRev.gemspec
@@ -9,10 +9,10 @@ Gem::Specification.new do |spec|
   spec.authors       = ["Dandush03"]
   spec.email         = ["d.laloush@outlook.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because RubyGems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
-  spec.license       = "MIT"
+  spec.summary       = 'Daniel Reviewer'
+  spec.description   = '{This Gem Review CSS Errors (For Now!)'
+  spec.homepage      = 'dlaloush.me'
+  spec.license       = 'MIT'
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
   # to allow pushing to a single host or delete this section to allow pushing to any host.
@@ -38,4 +38,8 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.17"
   spec.add_development_dependency "rake", "~> 10.0"
+
+  spec.add_runtime_dependency "main"
+
+  spec.add_runtime_dependency "main.rb"
 end
--- lib/DRev.rb
+++ lib/DRev.rb
@@ -2,5 +2,53 @@ require "DRev/version"
 
 module DRev
   class Error < StandardError; end
-  # Your code goes here...
+
+  class CLI < Main
+    # frozen_string_literal: true
+
+    # !/usr/bin/env ruby
+
+    require_relative '../lib/scanner.rb'
+    def init_read(file)
+      read = DRev::Scanner.new(file)
+      print read.scn
+    end
+
+    # rubocop: disable Metrics/AbcSize
+
+    def init_start(counter)
+      drev_str = "\n  Starting ".green
+      drev_str += 'DRev'.magenta.bold
+      drev_str += ' Inspecting '.green
+      drev_str += "#{counter} ".yellow.bold
+      drev_str += 'Files'.green if counter < 1
+      drev_str += 'File'.green if counter >= 1
+      drev_str += "\n"
+      drev_str
+    end
+
+    def init_end(counter)
+      drev_str = '  Finished '.green
+      drev_str += 'DRev'.magenta.bold
+      drev_str += ' Reviewing '.green
+      drev_str += "#{counter} ".yellow.bold
+      drev_str += 'Files'.green if counter < 1
+      drev_str += 'File'.green if counter >= 1
+      drev_str += "\n\n"
+      drev_str
+    end
+
+    # rubocop: enable Metrics/AbcSize
+
+    files = Dir.glob(File.join('**', '*.css')).reject { |f| f['spec/'] }
+    puts init_start(files.count)
+    counter = 0
+    files.each do |file|
+      counter += 1
+      puts ''
+      init_read(file)
+    end
+    puts init_end(counter)
+
+  end
 end
